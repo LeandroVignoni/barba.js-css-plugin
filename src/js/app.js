e93ecb71024ea087771f5bb5d1cdbcc3ee7a9e22
LVignoni <leandrovignoni@gmail.com>
2021-04-26 12:36:15 -0300

    fade + clip path

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,37 @@ import barbaCss from "@barba/css";
 
 barba.use(barbaCss);
 
+const body = document.querySelector("body");
+barba.hooks.before((data) => {
+  const bgColor = data.current.container.dataset.background;
+  body.style.setProperty("--page-background", bgColor);
+});
+
 barba.init({
-  trasitions: [
+  transitions: [
     {
+      name: "home",
+      beforeOnce() {
+        console.log("if we need to fire some script");
+      },
       once() {},
+    },
+    {
+      name: "fade",
+      to: {
+        namespace: ["fade"],
+      },
+      leave() {},
+      enter() {},
+    },
+    {
+      name: "clip",
+      sync: true,
+      to: {
+        namespace: ["clip"],
+      },
+      leave() {},
+      enter() {},
     },
   ],
 });
